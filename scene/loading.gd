4afafff94615b38071622e8932c035d869f757dd
NASAPsyche <psychemission@gmail.com>
2025-05-07 11:34:24 -0700

    moved build files to root folder

--- scene/loading.gd
+++
@@ -1,5 +0,0 @@
-extends PanelContainer
-
-func _process(delta: float):
-	if not $"../SamplePopup".visible:
-		self.visible = false
--- script/poi.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-@export var poi_name: String
-@export var poi_value: float
